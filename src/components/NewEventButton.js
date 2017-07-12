1b7b498a1491745f9851a3697474268c546d2a3d
Jacob Kayen <jacob.kayen@gmail.com>
2017-07-11 22:46:18 -0400

    change prompt button based on current activity

--- src/components/NewEventButton.js
+++ src/components/NewEventButton.js
@@ -2,10 +2,38 @@ import React from 'react';
 import { Text } from 'react-native';
 import { Link } from 'react-router-native';
 
-export default function NewEventButton() {
-  return (
-    <Link to={`/prompt`}>
-      <Text>What are you up to?</Text>
-    </Link>
-  )
+import getCurrentEvent from '../database/helpers/getCurrentEvent';
+import getEvent from '../database/helpers/getEvent';
+
+export default class NewEventButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentActivity: ''
+    }
+  }
+  componentWillMount() {
+    getCurrentEvent()
+    .then(getEvent)
+    .then(eventObj => {
+      if (eventObj) {
+        this.setState({
+          currentActivity: JSON.parse(eventObj).activityName
+        })
+      }
+    })
+  }
+  render() {
+    return (
+      <Link to={`/prompt`}>
+        <Text>
+          {this.state.currentActivity ? (
+            `No longer ${this.state.currentActivity}?`
+          ) : (
+            `What are you up to?`
+          )}
+        </Text>
+      </Link>
+    )
+  }
 }
